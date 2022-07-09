dd53d7c8973d2269497b9f761e2f0257dd8d7309
jensonjohnathon <108676720+jensonjohnathon@users.noreply.github.com>
2022-07-09 16:23:48 +0200

    Add files via upload

---
+++ demo/script.js
@@ -0,0 +1,10 @@
+function addTodo() {
+    document.getElementById("add").onclick  = function() {
+
+        var node = document.createElement("Li");
+        var text = document.getElementById("user_input").value; 
+        var textnode=document.createTextNode(text);
+        node.appendChild(textnode);
+        document.getElementById("list_item").appendChild(node);
+    }   
+}
