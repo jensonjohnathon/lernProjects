a3a9adc4b41f26fa8e776468cda0f421d8477cdf
jensonjohnathon <108676720+jensonjohnathon@users.noreply.github.com>
2022-07-11 15:06:01 +0200

    Add files via upload
    fully working with bug fixes(deleteAll was only clickable once)

--- todo-app/script.js
+++ todo-app/script.js
@@ -3,12 +3,8 @@ var countLi = 0;
 var countDelete = 0;
 
 document.getElementById("add").onclick  = function() {  //Event listener for the Button that adds todo Items into the List
-    countLi += 1;
-    countDelete -= 1;
-
-    var countLinode=document.createTextNode(countLi);
-    var countDeletenode=document.createTextNode(countDelete);
-
+    countLi += 1; // counter used to assign a different id to each List Item
+    countDelete -= 1; // counter used to assign a different id to each Checkbox
     // li code
     var node = document.createElement("Li"); //Creates a new list element 
     node.id = countLi.toString();
@@ -16,7 +12,6 @@ document.getElementById("add").onclick  = function() {  //Event listener for the
     var textnode=document.createTextNode(text); //turns the variable text into a textnode and saves it to the variable textnode
     node.appendChild(textnode); // adds the textnode into the node so it can be displayed in the li element
     // li code
-
     //checkbox code
     var checkbox = document.createElement('input'); // creates the checkbox this checkbox will be used to delete the todo item from the list of todo items
     //assigning attributes to the checkbox
@@ -25,31 +20,33 @@ document.getElementById("add").onclick  = function() {  //Event listener for the
     checkbox.value = "value";
     checkbox.id = countDelete.toString();
     //checkbox code end
-
     // to tell the user what the checkbox is for a label with clarification text will be created
     var label = document.createElement('label'); // label for the checkbox for clarification
     label.htmlFor = "id"; // label attribute
     label.appendChild(document.createTextNode('Done?')) //text inside the Label displayed after the Checkbox
     node.appendChild(checkbox); //adding both to the List element
     node.appendChild(label);
-
-    node.appendChild(countLinode);
-    node.appendChild(countDeletenode);
-
+    
     document.getElementById("item_list").appendChild(node); // adds the li element node into the item list
 }
 
 document.getElementById("deleteAll").onclick  = function() { // function that deletes item list with a checkbox marking them as done
     var countLi2 = 0; // used for idetentification of the List Element
-    console.log(countDelete);
+    console.log(countDelete)
     for (i = -1; i >= countDelete; --i) {  // for loop to iterate through the boxes i has to start at -1 because --1 will only be called after the first loop. This loop will look for checkboxes that are checked and thus marked as done
-        countLi2 += 1;
-        var currentBox = document.getElementById(i.toString());
+        countLi2 += 1; //counter for the current List Item
+        var currentBox = document.getElementById(i.toString()); //variables that make it easier to work with the Checkbox or List Item elements in the loop
         var currentItem = document.getElementById(countLi2.toString());
-        console.log(currentItem);
-        console.log(currentBox);
-        if(currentBox.checked === true) {
-            currentItem.remove()
+
+        if(currentBox === null) { // bypass for null values
+            console.log("null Value detected")
+        } else if (currentBox.checked === true){ // else if clause that evaluates if the checkbox has been marked as done if that is the case the corresponding List elemt is removed
+
+            currentItem.remove();
+        } else { // this gets triggered if a box is unchecked
+            console.log("box not marked as done")
         }
+        console.log(currentBox)
+        console.log(currentItem)
     }
 }
